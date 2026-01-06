d993f49c81976393729c03ebe7c930663b4ca567
leonardo2609 <dioniziosleonardo@gmail.com>
2026-01-06 13:54:44 -0300

    Requisição do metar via API

---
+++ src/api/airbus.js
@@ -0,0 +1,45 @@
+async function getAirportInformation() {
+  const inputIcao = document.getElementById("airport-ICAO").value.trim().toUpperCase();
+
+  if (inputIcao === "" || inputIcao.length !== 4) {
+    console.log("Validação: O ICAO deve ter exatamente 4 caracteres.");
+    return;
+  }
+
+  const btn = document.querySelector('.btn-api');
+  const originalText = btn.innerText;
+  btn.innerText = "LOADING...";
+  btn.disabled = true;
+
+  const webhookUrl = "https://automacao.csvp.g12.br/webhook/metar_airport";
+
+  try {
+    const response = await fetch(webhookUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        icao: inputIcao,
+      }),
+    });
+
+    if (!response.ok) throw new Error("Erro na requisição");
+    
+    const data = await response.json();
+    
+    console.log(data);
+
+    if (data.wdir !== null) document.querySelector('input[placeholder="Dir"]').value = data.wdir;
+    if (data.wspd !== null) document.querySelector('input[placeholder="Spd"]').value = data.wspd;
+    if (data.temp !== null) document.querySelector('input[placeholder="15"]').value = data.temp;
+    if (data.altim !== null) document.querySelector('input[placeholder="1013"]').value = data.altim;
+
+  } catch (error) {
+    console.error('Erro:', error);
+    alert('Não foi possível obter os dados do METAR.')
+  } finally {
+    btn.innerText = originalText;
+    btn.disabled = false;
+  };
+}
